9dba85bc08b26387bce87d62979b1e6209682af1
rsoltys <radovan.soltys@gmail.com>
2019-08-09 16:28:16 +0200

    layout header - small progress

--- inventor/views.py
+++ inventor/views.py
@@ -1,6 +1,5 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
-class HomeView(LoginRequiredMixin, TemplateView):
+class HomeView(TemplateView):
     template_name = 'home.html'
