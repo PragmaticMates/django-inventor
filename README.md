1de82456137ae0060286e5f7b36ffd0bdb751eb4
rsoltys <radovan.soltys@gmail.com>
2019-08-09 13:50:52 +0200

    add empty home view

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # django-inventor
 
 Directory listing Django app
+
+## build css
+cd inventor/assets/
+npm ci
+gulp watch
---
+++ inventor/views.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+
+
+class HomeView(LoginRequiredMixin, TemplateView):
+    template_name = 'home.html'
