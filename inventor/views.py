c0bbe677b8e04a52a504756928b79e3414df8a6d
rsoltys <radovan.soltys@gmail.com>
2020-02-22 16:25:08 +0100

    UI: home view - some progress

--- inventor/views.py
+++ inventor/views.py
@@ -1,5 +1,12 @@
 from django.views.generic import TemplateView
 
+from inventor.core.listings.models.general import Listing
+
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data.update({'top_listings': Listing.objects.all()})
+        return context_data
