cd0bce225ff3142bde4f5f80b783efc9fe6e6fa6
Erik Telepovský <erik.telepovsky@gmail.com>
2020-03-09 17:27:18 +0100

    refactoring generic listings list

--- inventor/core/listings/urls.py
+++ inventor/core/listings/urls.py
@@ -1,13 +1,22 @@
 from django.urls import path
-from django.utils.translation import pgettext_lazy
+from django.utils.text import slugify
+from django.utils.translation import pgettext_lazy, ugettext_lazy
 
-from inventor.core.listings.models.listing_types import Accommodation
+from inventor.core.listings.models.general import Listing
 from inventor.core.listings.views import ListingListView, ListingDetailView
 
 app_name = 'listings'
 
 urlpatterns = [
     path(pgettext_lazy('url', 'listings/'), ListingListView.as_view(), name='listing_list'),
-    path(pgettext_lazy('url', 'accommodations/'), ListingListView.as_view(model=Accommodation), name='accommodation_list'),
-    path(pgettext_lazy('url', '<str:slug>/'), ListingDetailView.as_view(), name='listing_detail'),
 ]
+
+# lists
+for listing_type in Listing.__subclasses__():
+    url_name = f'{listing_type.__name__.lower()}_list'
+    verbose_name_plural = ugettext_lazy(listing_type._meta.verbose_name_plural)
+    url_path = slugify(verbose_name_plural) + '/'
+    urlpatterns.append(path(url_path, ListingListView.as_view(template_name='listings/listing_list.html', model=listing_type), name=url_name))  # TODO: move to Listing class
+
+# listing detail
+urlpatterns.append(path(pgettext_lazy('url', '<str:slug>/'), ListingDetailView.as_view(), name='listing_detail'))
--- inventor/core/listings/views.py
+++ inventor/core/listings/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import ListView, DetailView
+
+from inventor.core.listings.models.listing_types import Accommodation
 from pragmatic.mixins import LoginPermissionRequiredMixin
 
 from inventor.core.listings.models.general import Listing
@@ -6,7 +8,16 @@ from inventor.core.listings.models.general import Listing
 
 class ListingListView(LoginPermissionRequiredMixin, ListView):
     model = Listing
+    test = Accommodation
     permission_required = 'listings.list_listing'
+    paginate_by = 12
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(*kwargs)
+        context_data.update({
+            'title': self.model._meta.verbose_name_plural,
+        })
+        return context_data
 
 
 class ListingDetailView(LoginPermissionRequiredMixin, DetailView):
