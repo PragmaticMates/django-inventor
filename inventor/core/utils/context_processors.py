d933a9d3e28ec2c330e10147862752f228ee622b
rsoltys <radovan.soltys@gmail.com>
2020-06-09 15:11:08 +0200

    VC UI: header - language switcher

--- inventor/core/utils/context_processors.py
+++ inventor/core/utils/context_processors.py
@@ -5,3 +5,11 @@ def deployment_timestamp(request):
     return {
         'deployment_timestamp': os.environ.get('DEPLOYMENT_TIMESTAMP', None)
     }
+
+
+def settings(request):
+    import django
+
+    return {
+        'settings': django.conf.settings
+    }
