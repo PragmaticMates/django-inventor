a0d85dba20b332299dd91a79f3a5c838e7ea6e25
Erik Telepovský <erik.telepovsky@gmail.com>
2020-06-23 21:58:12 +0200

    INVENTOR_LISTINGS_URL_ENABLED setting

--- inventor/core/listings/urls.py
+++ inventor/core/listings/urls.py
@@ -2,20 +2,26 @@ from django.urls import path
 from django.utils.text import slugify
 from django.utils.translation import pgettext_lazy, ugettext_lazy
 
+from inventor import settings as inventor_settings
 from inventor.core.listings.models.general import Listing
 from inventor.core.listings.views import ListingListView, ListingDetailView
 
 app_name = 'listings'
 
-urlpatterns = [
-    path(pgettext_lazy('url', 'listings/'), ListingListView.as_view(), name='listing_list'),
-]
+urlpatterns = []
+
+if inventor_settings.LISTINGS_URL_ENABLED:
+    urlpatterns.append(path(pgettext_lazy('url', 'listings/'), ListingListView.as_view(), name='listing_list'))
+
 
 # lists
-for listing_type in Listing.__subclasses__():
+for index, listing_type in enumerate(Listing.__subclasses__()):
     url_name = listing_type.get_list_url_name()
     url_path = listing_type.URL.slug
     urlpatterns.append(path(url_path, ListingListView.as_view(template_name='listings/listing_list.html', model=listing_type), name=url_name))
+
+    if not inventor_settings.LISTINGS_URL_ENABLED and len(inventor_settings.LISTING_TYPES) > 0 and listing_type.__name__ == inventor_settings.LISTING_TYPES[0]:
+        urlpatterns.append(path(url_path, ListingListView.as_view(template_name='listings/listing_list.html', model=listing_type), name='listing_list'))
 
 # listing detail
 urlpatterns.append(path(pgettext_lazy('url', '<str:slug>/'), ListingDetailView.as_view(), name='listing_detail'))
