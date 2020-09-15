aa2675fa6da8455e54dff07389c328677658167e
Erik Telepovský <erik.telepovsky@gmail.com>
2020-09-15 09:54:32 +0200

    translated accounts/ URL prefix

--- inventor/urls.py
+++ inventor/urls.py
@@ -3,13 +3,14 @@ from django.urls import path, include
 
 from inventor.core.newsletters.views import NewsletterView
 from inventor.views import HomeView
+from django.utils.translation import pgettext_lazy
 
 app_name = 'inventor'
 
 urlpatterns = (
     path('', HomeView.as_view(), name='home'),
     path('newsletter/', NewsletterView.as_view(), name='newsletter'),
-    path('accounts/', include('inventor.core.accounts.urls', namespace='accounts')),
+    path(pgettext_lazy('url', 'accounts/'), include('inventor.core.accounts.urls', namespace='accounts')),
 )
 
 if 'inventor.core.subscriptions' in settings.INSTALLED_APPS:
