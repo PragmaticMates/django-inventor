925a940c1216b468eaaf5c380fe9e3bc5d466587
rsoltys <radovan.soltys@gmail.com>
2020-12-28 12:06:50 +0100

    TT: Manager - demo urls

--- inventor/manager/urls.py
+++ inventor/manager/urls.py
@@ -5,12 +5,12 @@ from django.views.generic import TemplateView
 app_name = 'manager'
 
 urlpatterns = [
-    path(pgettext_lazy("url", ''), TemplateView.as_view(template_name='manager/index2.html'), name='manager_dashboard'),
-    path('admin-user-profile', TemplateView.as_view(template_name='admin/user-profile-lite.html'), name='admin_user_profile'),
-    path('admin-components-blogs', TemplateView.as_view(template_name='admin/components-blog-posts.html'), name='admin_components_blog_posts'),
-    path('admin-components-blogs', TemplateView.as_view(template_name='admin/components-blog-posts.html'), name='admin_components_blog_posts'),
-    path('admin-add-new-post', TemplateView.as_view(template_name='admin/add-new-post.html'), name='admin_add_new_post'),
-    path('admin-form-components', TemplateView.as_view(template_name='admin/form-components.html'), name='admin_form_components'),
-    path('admin-tables', TemplateView.as_view(template_name='admin/tables.html'), name='admin_tables'),
-    path('admin-errors', TemplateView.as_view(template_name='admin/errors.html'), name='admin_errors'),
+    path(pgettext_lazy("url", ''), TemplateView.as_view(template_name='manager/index2.html'), name='dashboard'),
+    path(pgettext_lazy("url", 'user-profile'), TemplateView.as_view(template_name='manager/user-profile-lite.html'), name='user_profile'),
+    path(pgettext_lazy("url", 'components-blogs'), TemplateView.as_view(template_name='manager/components-blog-posts.html'), name='components_blog_posts'),
+    path(pgettext_lazy("url", 'components-blogs'), TemplateView.as_view(template_name='manager/components-blog-posts.html'), name='components_blog_posts'),
+    path(pgettext_lazy("url", 'add-new-post'), TemplateView.as_view(template_name='manager/add-new-post.html'), name='add_new_post'),
+    path(pgettext_lazy("url", 'form-components'), TemplateView.as_view(template_name='manager/form-components.html'), name='form_components'),
+    path(pgettext_lazy("url", 'tables'), TemplateView.as_view(template_name='manager/tables.html'), name='tables'),
+    path(pgettext_lazy("url", 'errors'), TemplateView.as_view(template_name='manager/errors.html'), name='errors'),
 ]
