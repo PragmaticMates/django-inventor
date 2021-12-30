0c076fa286f7ff4fe73426790b9fbb2b430ad317
eriktelepovsky <erik.telepovsky@gmail.com>
2021-12-30 15:45:26 +0100

    get_choices_with_parents fix

--- inventor/templatetags/inventor.py
+++ inventor/templatetags/inventor.py
@@ -97,7 +97,7 @@ def get_choices_with_parents(**kwargs):
     if not isinstance(field, TreeNodeMultipleChoiceField) and not isinstance(field, TreeNodeChoiceField):
         return None
 
-    return dict(field.queryset.values_list('slug', 'parent'))
+    return dict(field.queryset.values_list('slug_i18n', 'parent'))  # TODO: slug is not universal identifier
 
 
 @register.filter()
